b8522f7d73d10a2f0363bfba43ba8657dc181cf1
Chris DeAngelis <12800772+Chris-DeAngelis@users.noreply.github.com>
2023-07-13 13:20:05 -0500

    Update Home.py

--- App/Home.py
+++ App/Home.py
@@ -119,12 +119,12 @@ def add_logo():
                 background-position: 20px 20px;
             }
             [data-testid="stSidebarNav"]::before {
-                content: "Spaulding Ridge";
+                content: "My Company Name";
                 margin-left: 20px;
                 margin-top: 20px;
-                font-size: 15px;
+                font-size: 30px;
                 position: relative;
-                top: 150px;
+                top: 100px;
             }
         </style>
         """,
