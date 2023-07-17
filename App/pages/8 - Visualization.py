9493d0e941d5dff0ae2cbc483276e5e877280c90
Chris DeAngelis <12800772+Chris-DeAngelis@users.noreply.github.com>
2023-07-17 13:59:32 -0500

    Update 8 - Visualization.py

--- App/pages/8 - Visualization.py
+++ App/pages/8 - Visualization.py
@@ -3,6 +3,18 @@ import pandas as pd
 #import numpy as np
 import pygwalker as pyg
 
+st.set_page_config(
+    page_title="Data Science @ Spaulding Ridge | Chris DeAngelis, 2023",
+    page_icon=":bar_chart:",
+    layout="wide"#,
+    #initial_sidebar_state="collapsed"
+    #menu_items={
+    #    'Get Help': 'chris.deangelis@elkay.com'#,
+        #'Report a bug': "https://www.extremelycoolapp.com/bug",
+        #'About': "# This is a header. This is an *extremely* cool app!"
+    #}    
+)
+
 df = pd.read_csv(
    'https://raw.githubusercontent.com/Chris-DeAngelis/SpauldingRidge/main/supermarket_sales%20-%20Sheet1.csv',
    usecols = ['Branch','City','Customer type','Gender','Product line','Unit price','Quantity','Total','Date','Time','Payment','gross income'],
@@ -10,3 +22,5 @@ df = pd.read_csv(
 )
 
 gwalker = pyg.walk(df)
+# Embed the HTML into the Streamlit app
+components.html(gwalker, height=1000, scrolling=True)
