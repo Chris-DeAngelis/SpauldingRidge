78df91776e933cfe33e2ebb011b5b72efeca581b
Chris DeAngelis <12800772+Chris-DeAngelis@users.noreply.github.com>
2023-07-18 15:16:39 -0500

    Update 9 - 🚦Master Data Management.py

--- App/pages/9 - 🚦Master Data Management.py
+++ App/pages/9 - 🚦Master Data Management.py
@@ -18,7 +18,7 @@ data = [['Brian', 'Joesphson', '32 S Drury Lane, VA 27343', '(253)234-2122','mys
         ['Sally', 'Fields', '1232 N Hampton, NY 22340', '(153)256-0010','petfood.com',True]]
   
 # Create the pandas DataFrame
-df = pd.DataFrame(data, columns=['First Name','Last Name','Address','Phone #','Website','Primary Customer Flag','Mark For Review'])
+df = pd.DataFrame(data, columns=['First Name','Last Name','Address','Phone #','Website','Mark For Review'])
 
 # df = pd.DataFrame(
 #     [
