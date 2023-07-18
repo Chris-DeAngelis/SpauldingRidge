1cfa6a0655981c8cc8caf637e6dcd3a7ba8b419f
Chris DeAngelis <12800772+Chris-DeAngelis@users.noreply.github.com>
2023-07-18 12:26:29 -0500

    Update Home.py

--- App/Home.py
+++ App/Home.py
@@ -113,6 +113,8 @@ def add_logo():
             [data-testid="stSidebarNav"] {
                 background-image: url(https://github.com/Chris-DeAngelis/SpauldingRidge/blob/main/App/SR%20Logo.PNG?raw=true);
                 background-repeat: no-repeat;
+                padding-top: 25px;
+                padding-left: 25px;
                 background-position: 50px 50px;
                 background-size: 250px 125px;
             }
