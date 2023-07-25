981e1119ff4211a9e0c519a84df86c5eeba48771
Chris DeAngelis <12800772+Chris-DeAngelis@users.noreply.github.com>
2023-07-25 15:06:17 -0500

    Update 10 - 😡Sentiment Analysis.py

--- App/pages/10 - 😡Sentiment Analysis.py
+++ App/pages/10 - 😡Sentiment Analysis.py
@@ -19,7 +19,7 @@ st.set_page_config(
 #################### Page Content ####################
 st.title('Ratings & Reviews Demo')
 st.write('Securely connect to your data and use this template for extracting more insights out of your product reviews')
-st.caption('This is a sample of Amazon reviews: https://www.kaggle.com/datasets/yasserh/amazon-product-reviews-dataset?resource=download
+st.caption('This is a sample of Amazon reviews: https://www.kaggle.com/datasets/yasserh/amazon-product-reviews-dataset?resource=download')
 st.caption('The sentiment analysis uses a BERT NLP model from HuggingFace https://huggingface.co/nlptown/bert-base-multilingual-uncased-sentiment/tree/main')           
 
 #################### Preprocessing ####################
@@ -41,18 +41,6 @@ model_results = pd.DataFrame.from_dict(nlp_pipeline(data['Review Text']), orient
 data['ML Model'] = model_results['label'].str[:1].astype('int')
 st.write("Add in a ML Model")
 st.dataframe(data.head())
-#specify task
-#task = 'sentiment-analysis'
-
-# Instantiate pipeline
-#analyzer = pipeline(task, model=model)
-
-# Store output of the analysis
-#output = analyzer(input_text)
-
-# Return output
-#output
-
 
 #################### Page Logo ####################
 def add_logo():
