ddc2c59429ab7499285fd7bf0dc0be1e64fb6687
Chris DeAngelis <12800772+Chris-DeAngelis@users.noreply.github.com>
2023-07-25 14:23:15 -0500

    Update 10 - 😡Sentiment Analysis.py

--- App/pages/10 - 😡Sentiment Analysis.py
+++ App/pages/10 - 😡Sentiment Analysis.py
@@ -19,7 +19,8 @@ st.set_page_config(
 #################### Page Content ####################
 st.title('Ratings & Reviews Demo')
 st.write('Securely connect to your data and use this template for extracting more insights out of your product reviews')
-st.caption('This is a sample of Amazon reviews: https://www.kaggle.com/datasets/yasserh/amazon-product-reviews-dataset?resource=download. The sentiment analysis uses a BERT NLP model from HuggingFace https://huggingface.co/nlptown/bert-base-multilingual-uncased-sentiment/tree/main')
+st.caption('This is a sample of Amazon reviews: https://www.kaggle.com/datasets/yasserh/amazon-product-reviews-dataset?resource=download)
+st.caption('The sentiment analysis uses a BERT NLP model from HuggingFace https://huggingface.co/nlptown/bert-base-multilingual-uncased-sentiment/tree/main')           
 
 #################### Preprocessing ####################
 # Load sample Amazon review data
@@ -33,11 +34,8 @@ data.rename(columns={'name': 'Product',
 data = data[['Review Date','Product','Review Title','Review Text','Review Rating']]
 st.dataframe(data.head())
 
-
-
 #################### Load Pre-trained Sentiment Analysis Model ####################
-# nlp_model = pipeline(task='sentiment-analysis', 
-#                      model='nlptown/bert-base-multilingual-uncased-sentiment')
+nlp_pipeline = pipeline(task='sentiment-analysis', model='nlptown/bert-base-multilingual-uncased-sentiment')
 # model_results = pd.DataFrame.from_dict(nlp_pipeline(data['Review Text']), orient='columns')
 # data['ML Model'] = model_results['label'].str[:1].astype('int')
 # st.write("Add in a ML Model")
