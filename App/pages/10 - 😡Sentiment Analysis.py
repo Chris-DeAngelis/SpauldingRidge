3f3574def40f37ad52b8c5474980a1674d6e09d4
Chris DeAngelis <12800772+Chris-DeAngelis@users.noreply.github.com>
2023-07-25 13:05:09 -0500

    Update 10 - 😡Sentiment Analysis.py

--- App/pages/10 - 😡Sentiment Analysis.py
+++ App/pages/10 - 😡Sentiment Analysis.py
@@ -19,12 +19,12 @@ st.set_page_config(
 st.title('Ratings & Reviews Demo')
 st.write('Securely connect to your data and use this template for extracting more insights out of your product reviews')
 
-#data = pd.read_csv('https://raw.githubusercontent.com/Chris-DeAngelis/SpauldingRidge/main/product_reviews.csv',
-#                   usecols=['name','dateUpdated','reviews.rating','reviews.text','reviews.title'])#'categories',
-st.dataframe(df.head())
+data = pd.read_csv('https://raw.githubusercontent.com/Chris-DeAngelis/SpauldingRidge/main/product_reviews.csv',
+                   usecols=['name','dateUpdated','reviews.rating','reviews.text','reviews.title'])#'categories',
+st.dataframe(data.head())
 
 #################### Load Pre-trained Sentiment Analysis Model ####################
-#model = hf_hub_download(repo_id="nlptown/bert-base-multilingual-uncased-sentiment", filename="tf_model.h5")
+model = hf_hub_download(repo_id="nlptown/bert-base-multilingual-uncased-sentiment", filename="tf_model.h5")
 
 # Load pre-trained model
 #model = 'bert-base-multilingual-uncased-sentiment'
